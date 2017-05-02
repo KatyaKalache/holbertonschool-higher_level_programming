6e057adeac5259eade278a49a15979ebdd04530e
KatyaKalache <katya@kalache.fr>
2017-05-02 20:38:00 +0000

    changing to else last case

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,5 +11,5 @@ if ld > 5:
 elif ld < 6 and ld != 0:
     print('Last digit of {:d} is {:d} and is less than 6 and\
   not 0'.format(number, ld))
-elif ld == 0:
+else:
     print('Last digit of {:d} is {:d} and is 0'.format(number, ld))
