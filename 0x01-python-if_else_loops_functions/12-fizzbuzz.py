a78640ff5cc5ef7bc68a328e140734d0df3b3452
KatyaKalache <katya@kalache.fr>
2017-05-03 05:16:51 +0000

    pep fix

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     number = 1
     while number <= 100:
-        if number % 3 == 0 and number % 5 !=0:
+        if number % 3 == 0 and number % 5 != 0:
             print('Fizz ', end='')
         elif number % 5 == 0 and number % 3 != 0:
             print('Buzz ', end='')
