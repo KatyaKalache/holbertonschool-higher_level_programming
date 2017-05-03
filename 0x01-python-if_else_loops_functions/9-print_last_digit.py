2ae5fee270c2d5163a79050428539f1fd92b0e45
KatyaKalache <katya@kalache.fr>
2017-05-03 03:54:50 +0000

    initial

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    ld = number % 10
+    if number < 0:
+        number = number * -1
+        print('{:d}'.format(ld), end='')
+    else:
+        print('{:d}'.format(ld), end='')
+        return ld
