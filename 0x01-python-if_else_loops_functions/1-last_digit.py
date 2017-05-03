417204467ae783f5756786afd90f169ec0f1cd66
KatyaKalache <katya@kalache.fr>
2017-05-03 06:07:24 +0000

    tabs

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,7 +9,7 @@ if ld == 0:
     print('Last digit of {:d} is {:d} and is 0'.format(number, ld))
 if ld > 5:
     print('Last digit of {:d} is {:d} and is greater\
-  than 5'.format(number, ld))
+ than 5'.format(number, ld))
 else:
     print('Last digit of {:d} is {:d} and is less than 6 and\
  not 0'.format(number, ld))
