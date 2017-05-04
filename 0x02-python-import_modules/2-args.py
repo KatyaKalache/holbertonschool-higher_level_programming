86f9ba62d804ba40664319107295adc548654b50
KatyaKalache <katya@kalache.fr>
2017-05-04 21:34:32 +0000

    adjusting spaces

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
         print('{:d} argument.'.format(argv_num - 1))
     elif argv_num == 2:
         print('{:d} argument:\n{:d}:'.format(argv_num - 1, argv_num - 1),
- str(sys.argv[1]))
+              str(sys.argv[1]))
     elif argv_num > 2:
         print('{:d} arguments:'.format(argv_num - 1))
         for i in range(1, argv_num):
