f9da8952f0b38e41c8e1c72c067c14bcba85815d
KatyaKalache <katya@kalache.fr>
2017-05-04 21:46:37 +0000

    removing space after count by printing string using {:s}

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
         print('{:d} argument:\n{:d}:'.format(argv_num - 1, argv_num - 1),
               str(sys.argv[1]))
     elif argv_num > 2:
-        print('{:d} arguments:'.format(argv_num - 1))
-        for i in range(1, argv_num):
-            print(i, ':', str(sys.argv[i]))
+            print('{:d} arguments:'.format(argv_num - 1))
+            for i in range(1, argv_num):
+                print('{:d}: {:s}'.format(i, sys.argv[i]))
