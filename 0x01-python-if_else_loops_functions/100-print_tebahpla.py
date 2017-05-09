92b2381e05c71ceba06ea78635d87261ec04dc2b
KatyaKalache <katya@kalache.fr>
2017-05-09 17:41:28 +0000

    using ascii value method

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a') - 1, -1):
-        if (i % 2) != 0:
-                i = i - 32
-        print('{:s}'.format(chr(i)), end='')
+i = 122
+while i > 96:
+    copy = i
+    if (i % 2) != 0:
+        copy = i - 32
+    print('{:}'.format(chr(copy)), end='')
+    i = i - 1
