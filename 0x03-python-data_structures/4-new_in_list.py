5f59c4cfe142e114b5419ce2d7936442d1e88f1b
KatyaKalache <katya@kalache.fr>
2017-05-10 00:18:53 +0000

    switching loops

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     copy_list = my_list[:]
-    if idx <= len(my_list) and idx > 0:
-        copy_list[idx] = element
+    if idx >= len(my_list) or idx < 0:
         return copy_list
-    else:
-        return copy_list
+    copy_list[idx] = element
+    return copy_list
