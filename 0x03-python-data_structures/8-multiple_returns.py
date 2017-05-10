989adfb665d1e433b858013008f8f9504b46a936
KatyaKalache <katya@kalache.fr>
2017-05-10 05:38:00 +0000

    swapping

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is None:
-        return 0, None
+    if sentence != None:
+        return len(sentence), sentence[0]
     else:
-        first_char = sentence[0]
-        return len(sentence), first_char
+        return 0, None
