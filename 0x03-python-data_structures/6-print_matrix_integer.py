4aae6b781c764e03392b8472b87aedef42bd1585
KatyaKalache <katya@kalache.fr>
2017-05-10 22:20:58 +0000

    adding :d

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,4 @@ def print_matrix_integer(matrix=[[]]):
     if matrix is None:
         return None
     for row in matrix:
-        print(" ".join("{}".format(cols) for cols in row))
+        print(" ".join("{:d}".format(cols) for cols in row))
