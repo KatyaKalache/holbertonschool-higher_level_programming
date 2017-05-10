85763798a1ad895b5c19eacdbd22085c196cb8d0
KatyaKalache <katya@kalache.fr>
2017-05-10 23:11:38 +0000

    - semicolon and if list == ''

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list is None:
+    if my_list == "":
         return None
     else:
-        my_list.sort();
-        my_list.reverse();
+        my_list.sort()
+        my_list.reverse()
         max = my_list[0]
         return max
