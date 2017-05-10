144480834bfd5ea1722bb57beb30940f487d7278
KatyaKalache <katya@kalache.fr>
2017-05-10 04:29:43 +0000

    if len of matrix is 0

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,9 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix is None:
         return
+    if len(matrix[0]) == 0 and len(matrix) == 1:
+        print("")
+        return
     for rows in matrix:
         for cols in rows:
             print("{:d}".format(cols), end="")
