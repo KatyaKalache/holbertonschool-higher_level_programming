56efb71d03eb4ce21f48dc3caeb039ec284cba80
KatyaKalache <katya@kalache.fr>
2017-05-10 06:00:42 +0000

    check if list does not exist

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_int = 0
-    for i, val in enumerate(my_list):
-        if val > max_int:
-            max_int = val
-    return max_int
+    if my_list is None:
+        return None
+    else:
+        max_int = 0
+        for i, val in enumerate(my_list):
+            if val > max_int:
+                max_int = val
+        return max_int
