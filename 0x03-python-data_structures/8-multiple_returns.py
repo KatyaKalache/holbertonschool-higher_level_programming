f8f8944dccc9b0f7197dbfaabc4259cb3f280684
KatyaKalache <katya@kalache.fr>
2017-05-10 05:29:51 +0000

    assigning len(snetence) to length

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is None:
-        return
     first_char = sentence[0]
-    if len(sentence) == 0:
+    length = len(sentence)
+    if length == 0:
         first_char = None
-    return(len(sentence), first_char)
+    return(length, first_char)
