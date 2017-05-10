53ca195bb2fd350afbaa30d6c81a7074a22aa71f
KatyaKalache <katya@kalache.fr>
2017-05-10 05:21:06 +0000

    first chat = none

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,5 +2,5 @@
 def multiple_returns(sentence):
     first_char = sentence[0]
     if sentence is None:
-        first_char is None
+        first_char = "None"
     return(len(sentence), first_char)
