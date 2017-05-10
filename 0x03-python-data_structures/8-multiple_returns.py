16da5fe3bf43df216dedd182af9b9db0a6487c02
KatyaKalache <katya@kalache.fr>
2017-05-10 05:39:30 +0000

    is not none

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence != None:
+    if sentence is not None:
         return len(sentence), sentence[0]
     else:
         return 0, None
