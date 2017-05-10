ebc2e1c74195ef7b7266adc8364abec507083806
KatyaKalache <katya@kalache.fr>
2017-05-10 05:27:02 +0000

    sentence is empty

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,5 +4,5 @@ def multiple_returns(sentence):
         return
     first_char = sentence[0]
     if len(sentence) == 0:
-        first_char = "None"
+        first_char = None
     return(len(sentence), first_char)
