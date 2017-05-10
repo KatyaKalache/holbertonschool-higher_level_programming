9d81cf22e884c63ea7f5ea50e5a8c859093284a7
KatyaKalache <katya@kalache.fr>
2017-05-10 05:36:11 +0000

    return 0, none

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    first_char = sentence[0]
-    length = len(sentence)
-    if length == 0:
-        first_char = None
-    return(length, first_char)
+    if sentence is None:
+        return 0, None
+    else:
+        first_char = sentence[0]
+        return len(sentence), first_char
