8e87dcb4df862d80d48d9d83ba36be0198e0c118
KatyaKalache <katya@kalache.fr>
2017-05-10 21:38:14 +0000

    using join

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix is None:
-        return
-    if len(matrix[0]) == 0 and len(matrix) == 1:
-            print("")
-            return
-    for rows in matrix:
-        for cols in rows:
-            print("{:d}".format(cols), end="")
-            if cols % 3 != 0:
-                print(" ", end="")
-        print("")
+        return None
+    for row in matrix:
+        print(" ".join("{}".format(cols) for cols in row))
