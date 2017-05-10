89f698182903c4c6d58c1a5f09ca24614c778c3e
KatyaKalache <katya@kalache.fr>
2017-05-10 04:16:39 +0000

    if matrix none return none

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix is None:
+        return None
     for rows in matrix:
         for cols in rows:
             print("{:d}".format(cols), end="")
