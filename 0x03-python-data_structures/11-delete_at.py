ead955b7545eaad473a1b170c273f777ffe2e187
KatyaKalache <katya@kalache.fr>
2017-05-11 01:02:43 +0000

    accounting for negative, [], removing return none

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if my_list is not None or my_list != []:
+    if idx >= len(my_list) or my_list == [] or idx < 0:
+        return my_list
+    else:
+#        new_list = my_list[:]
         del (my_list[idx])
         return  my_list
-    else:
-        return None
