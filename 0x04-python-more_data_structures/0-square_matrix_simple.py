579f44947202a8624d7f76900afcd7bfef5ad5d6
KatyaKalache <katya@kalache.fr>
2017-05-11 20:13:13 +0000

    using append

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    for i in matrix:
-        return(i**2)
+    squares = []
+    for row in matrix:
+        squares.append([i*i for i in row])
+    return squares
