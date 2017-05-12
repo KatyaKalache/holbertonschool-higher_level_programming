f872909d0aa25729a9dc3a547ff2378097e51e4e
KatyaKalache <katya@kalache.fr>
2017-05-12 05:22:33 +0000

    adding :s

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(my_dict):
     for key, element in sorted(my_dict.items()):
-        s = ("{} : {}".format(key, element))
+        s = ("{:s} : {}".format(key, element))
         print(s)
