f9b2dfc7a2bcfb531cf7b8fd7e641bc7887bb7b2
KatyaKalache <katya@kalache.fr>
2017-05-12 22:42:36 +0000

    initial

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+def safe_print_list(my_list=[], x=0):
+    if x >= 0:
+        try:
+            for i in range(x):
+                print("{:d}".format(my_list[i]), end="")
+            print()
+            return x
+        except IndexError:
+            pass
+            print()
+            return x-2
