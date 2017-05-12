23f4aa99701976a53c18b1cf14a552be5c794356
KatyaKalache <katya@kalache.fr>
2017-05-12 02:11:52 +0000

    substraction instead

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    new_set = []
-    for i in set_1:
-        if i in set_2:
-            new_set.append(i)
-            return new_set
+    new_set = set(set_1) - (set(set_1) - set( set_2))
+    return new_set
