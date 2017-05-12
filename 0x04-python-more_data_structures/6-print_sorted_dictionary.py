493b1af1209375d8fb25124edf800f63a2d5db7c
KatyaKalache <katya@kalache.fr>
2017-05-12 17:21:03 +0000

    removing space before colomn

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(my_dict):
     for key, value in sorted(my_dict.items()):
-        s = ("{:s} : {}".format(key, value))
+        s = ("{:s}: {}".format(key, value))
         print(s)
