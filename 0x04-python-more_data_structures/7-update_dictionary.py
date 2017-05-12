ab8e3daa8aea583eb0246a51ae5f9ef8178cf6ed
KatyaKalache <katya@kalache.fr>
2017-05-12 01:14:01 +0000

    rm copy

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(my_dict, key, value):
-    new_dict = my_dict.copy()
-    new_dict[key] = value
-    return new_dict
+    my_dict[key] = value
+    return my_dict
