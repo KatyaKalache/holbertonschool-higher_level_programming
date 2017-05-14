f6ccd3dc206e28c29277fde15478691fcf4d1803
KatyaKalache <katya@kalache.fr>
2017-05-14 00:01:50 +0000

    if list empty return 0

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if my_list == []:
+        return 0
     div = sum(i[1] for i in my_list)
     mul = [(i[0] * i[1]) for i in my_list]
     total = sum(mul)
