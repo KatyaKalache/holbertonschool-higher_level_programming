c69184d17b19ad4121a7a9650fc1ca7e0fe21c84
KatyaKalache <katya@kalache.fr>
2017-05-17 16:08:45 +0000

    rm l.strip

--- 0x06-python-test_driven_development/5-text_indentation.py
+++ 0x06-python-test_driven_development/5-text_indentation.py
@@ -3,9 +3,9 @@ def text_indentation(text):
    if not isinstance(text, str):
       raise TypeError("text must be a string")
    [i.strip for i in text.split(".")]
-   text = text.replace(". ",".\n\n").lstrip()
+   text = text.replace(". ",".\n\n")
    [i.strip for i in text.split("?")]
-   text = text.replace("? ","?\n\n").lstrip()
+   text = text.replace("? ","?\n\n")
    [i.strip for i in text.split(":")]
-   text = text.replace(": ",":\n\n").lstrip()
+   text = text.replace(": ",":\n\n")
    print(text)
