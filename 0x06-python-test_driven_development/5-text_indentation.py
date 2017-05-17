da164fb8cb4d67d40a6a8adfebe2e97b764ab10d
KatyaKalache <katya@kalache.fr>
2017-05-17 06:28:35 +0000

    removing spaces

--- 0x06-python-test_driven_development/5-text_indentation.py
+++ 0x06-python-test_driven_development/5-text_indentation.py
@@ -3,9 +3,9 @@ def text_indentation(text):
    if not isinstance(text, str):
       raise TypeError("text must be a string")
    [i.strip for i in text.split(".")]
-   text = text.replace(".",".\n"+"\n")
+   text = text.replace(". ",".\n\n").lstrip()
    [i.strip for i in text.split("?")]
-   text = text.replace("?","?\n"+"\n")
+   text = text.replace("? ","?\n\n").lstrip()
    [i.strip for i in text.split(":")]
-   text = text.replace(":",":\n"+"\n")
+   text = text.replace(": ",":\n\n").lstrip()
    print(text)
