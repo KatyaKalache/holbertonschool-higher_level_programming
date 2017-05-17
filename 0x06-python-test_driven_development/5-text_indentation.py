4c0e9a8c4d5ddfd415a8622e6bb745224aed8c7a
KatyaKalache <katya@kalache.fr>
2017-05-17 18:21:35 +0000

    editing tabs for 4 spaces

--- 0x06-python-test_driven_development/5-text_indentation.py
+++ 0x06-python-test_driven_development/5-text_indentation.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def text_indentation(text):
-   if not isinstance(text, str):
-      raise TypeError("text must be a string")
-   [i.strip for i in text.split(".")]
-   text = text.replace(". ",".\n\n")
-   text = text.replace(".",".\n\n")
-   [i.strip for i in text.split("?")]
-   text = text.replace("? ","?\n\n")
-   text = text.replace("?","?\n\n")
-   [i.strip for i in text.split(":")]
-   text = text.replace(": ",":\n\n")
-   text = text.replace(":",":\n\n")
-   print(text)
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    [i.strip for i in text.split(".")]
+    text = text.replace(". ",".\n\n")
+    text = text.replace(".",".\n\n")
+    [i.strip for i in text.split("?")]
+    text = text.replace("? ","?\n\n")
+    text = text.replace("?","?\n\n")
+    [i.strip for i in text.split(":")]
+    text = text.replace(": ",":\n\n")
+    text = text.replace(":",":\n\n")
+    print(text)
