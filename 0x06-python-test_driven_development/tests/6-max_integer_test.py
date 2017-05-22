446434ee17ff1d1da5b33993a9124b3ca7bcb0de
KatyaKalache <katya@kalache.fr>
2017-05-22 05:50:51 +0000

    redoing the whole ex, testing for max int in a list

--- 0x06-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x06-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,26 +4,8 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
-class TestMaxInteger(unittest.TestCase):
-
-    def max_integer(self):
-        values = [1, 2, 3]
-        result = max_integer(self)
-        self.assertEqual(result, 3)
-
-    def max_integer(self):
-        values = [-1, -2, -3]
-        result = max_integer(self)
-        self.assertEqual(result, -1)
-
-    def max_integer(self):
-        values = ["hello", -2, -3]
-        result = max_integer(self)
-        self.assertRaises("TypeError")
-
-    def max_integer(self):
-        values = None
-        self.assertRaises("Value Error")
+def test_int_list(self):
+    self.assertEqual(max_integer([1, 2, 3], 3))
 
 if __name__ == '__main__':
     unittest.main()
