b31cf5fc3bd5f7ed84cd87b375618235fa9fea46
KatyaKalache <katya@kalache.fr>
2017-05-22 06:24:08 +0000

    testing floats

--- 0x06-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x06-python-test_driven_development/tests/6-max_integer_test.py
@@ -9,5 +9,8 @@ class TestMaxInteger(unittest.TestCase):
     def test_int_negative(self):
         self.assertEqual(max_integer([-2, -1, -3]), -1)
 
+    def test_floats(self):
+        self.assertEqual(max_integer([1.2, 1.3, 1.4]), 1.4)
+
 if __name__ == '__main__':
     unittest.main()
