8f3dd455ddfe117f66b498f3492d40ca96e4ba7d
KatyaKalache <katya@kalache.fr>
2017-05-23 06:47:52 +0000

    if value is < 0

--- 0x07-python-classes/6-square.py
+++ 0x07-python-classes/6-square.py
@@ -22,7 +22,7 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if type(value) is not (int, int):
+        if type(value) is not (int, int) and value < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
