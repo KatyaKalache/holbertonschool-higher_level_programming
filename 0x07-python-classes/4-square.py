124e6b41cbce35a6f053780a19118c6f8ba45042
KatyaKalache <katya@kalache.fr>
2017-05-23 06:02:54 +0000

    pep fix

--- 0x07-python-classes/4-square.py
+++ 0x07-python-classes/4-square.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 class Square:
     def __init__(self, size=0):
-        self.__size = size
-    def area(self):
-        return self.__size ** 2
+        self.size = size
+
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, value):
         if type(value) is not int:
@@ -15,3 +15,6 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
+    def area(self):
+        return self.size ** 2
