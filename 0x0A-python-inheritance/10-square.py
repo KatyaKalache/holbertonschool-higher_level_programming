5da706447d7a7cbdff8db80b4f17707b2692989e
KatyaKalache <katya@kalache.fr>
2017-05-31 06:58:58 +0000

    pep

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
     def __init__(self, size):
         self.__size = size
@@ -11,4 +12,5 @@ class Square(Rectangle):
         return result
 
     def __str__(self):
-        return("[{:s}] {}/{}".format(type(self).__name__, self.__size, self.__size))
+        return("[{:s}] {}/{}".format(type(self).__name__, self.__size,
+                                     self.__size))
