6ae3dac41f8303fed327123dced7e239f2bcc8a2
KatyaKalache <katya@kalache.fr>
2017-05-31 05:17:58 +0000

    initial

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    def __init__(self, size):
+        self.__size = size
+        Rectangle.integer_validator(self, "size", size)
+
+    def area(self):
+        result = self.__size * self.__size
+        return result
+
+    def __str__(self):
+        return("[Rectangle] {}/{}".format(self.__size, self.__size))
