255732d2cc5305f461777b429edd53eae6261797
KatyaKalache <katya@kalache.fr>
2017-06-03 19:15:40 +0000

    another filename instead of actual name

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+"""
+writes a string to a text file (UTF8)
+returns the number of characters written
+"""
+
+
 def write_file(filename="", text=""):
     count = 0
     with open(filename, "w",  encoding='UTF-8') as f:
         f.write(text)
 
-    with open("my_first_file.txt", "r") as f:
+    with open(filename, "r") as f:
         for line in f:
             for nb_characters in line:
                 count += 1
