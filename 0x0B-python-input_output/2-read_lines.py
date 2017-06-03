00ec50d18b83b2b94dab5056173c88b57ab9c851
KatyaKalache <katya@kalache.fr>
2017-06-03 18:48:56 +0000

    changing to filename instead of actual name

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def read_lines(filename="", nb_lines=0):
     count = 0
-    with open("my_file_0.txt", encoding='UTF-8') as f:
+    with open(filename, encoding='UTF-8') as f:
         for line in f:
             count += 1
             if nb_lines < 1 or nb_lines >= count:
