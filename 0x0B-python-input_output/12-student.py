e8b4defaad3c6359e1469dd06b923f429858d9ca
KatyaKalache <katya@kalache.fr>
2017-06-03 21:39:24 +0000

    adding getattr, and search in range(len)

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import json
 """
-For attrs in list of strings, only attributes name from list must be retrieved.
+For attrs in list of strings,
+only attributes name from list must be retrieved.
 """
 
 
@@ -12,15 +12,12 @@ class Student:
             self.age = age
 
     def to_json(self, attrs=None):
-        match = []
+        match = {}
         if attrs is not None:
-            for i in attrs:
-#                print(i)
-                if hasattr(self, i):
-                    match.append(i)                  
-            return json.loads(json.dumps(match))
-       
-             #   else:
-              #      return json.loads(json.dumps(self.__dict__, i))
+            for i in range(len(attrs)):
+                if hasattr(self, attrs[i]):
+                    match[attrs[i]] = getattr(self, attrs[i])
+            return match
+
         if attrs is None:
-            return json.loads(json.dumps(self.__dict__, attrs))
+            return self.__dict__
