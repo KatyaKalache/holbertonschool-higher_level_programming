69bafb328df45138abd804f06ff616fdfa2512ce
KatyaKalache <katya@kalache.fr>
2017-06-03 18:46:03 +0000

    changing to filename instead of actual name

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    with open("my_file_0.txt", encoding='UTF-8') as f:
+    with open(filename, encoding='UTF-8') as f:
         for line in f:
             print(line, end="")
