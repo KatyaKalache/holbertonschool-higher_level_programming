76fd8121b773b4a0e2ff7a892c7d175433473303
KatyaKalache <katya@kalache.fr>
2017-06-04 04:26:10 +0000

    initial

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+For attrs in list of strings,
+only attributes name from list must be retrieved.
+"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+            self.first_name = first_name
+            self.last_name = last_name
+            self.age = age
+
+    def to_json(self, attrs=None):
+        match = {}
+        if attrs is not None:
+            for i in range(len(attrs)):
+                if hasattr(self, attrs[i]):
+                    match[attrs[i]] = getattr(self, attrs[i])
+            return match
+
+        if attrs is None:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        if hasattr(json, "first_name"):
+            setattr(self.first_name, first_name)
+        if hasattr(json, "last_name"):
+            setattr(self.last_name, last_name)
+        if hasattr(json, "age"):
+            setattr(self.age, age)
