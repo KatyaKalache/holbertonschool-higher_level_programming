5ffb462bfdd9e3adc78988a7185ef395d6a09544
KatyaKalache <katya@kalache.fr>
2017-07-13 01:29:35 +0000

    initial

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,2 @@
+-- creates a table second_table, create new records
+INSERT INTO second_table (`id`, `name`, `score`) VALUES ('1', 'John', '10' ), ('2', 'Alex', '3'), ('3', 'Bob', '14'), ('4', 'George', '8');
