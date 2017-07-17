dbe350daf93c929869ecd9da667115540b958f8a
KatyaKalache <katya@kalache.fr>
2017-07-17 06:08:27 +0000

    order by id replace by order by cities.id

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 --  lists all cities contained in the database
 SELECT cities.id, cities.name, states.name FROM cities, states
-       WHERE cities.id=states.id ORDER BY id ASC;
+       WHERE cities.id=states.id ORDER BY cities.id ASC;
