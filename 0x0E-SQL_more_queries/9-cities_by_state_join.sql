6ab132c1a2ff6c56f5b6661badb06a11f71bf148
KatyaKalache <katya@kalache.fr>
2017-07-17 06:11:04 +0000

    cities.id to cities.states_id

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 --  lists all cities contained in the database
 SELECT cities.id, cities.name, states.name FROM cities, states
-       WHERE cities.id=states.id ORDER BY cities.id ASC;
+       WHERE cities.state_id=states.id ORDER BY cities.id ASC;
