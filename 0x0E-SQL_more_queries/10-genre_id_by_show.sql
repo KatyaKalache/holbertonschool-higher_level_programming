76edb0842b141e0b89b4c855409bc10739784dc3
KatyaKalache <katya@kalache.fr>
2017-07-17 06:40:41 +0000

    adding where shows id eq genres id

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,5 @@
 -- lists all shows contained in hbtn_0d_tvshows
 SELECT tv_shows.title, tv_show_genres.genre_id, states.name
        FROM tv_shows, tv_shows_genres
+       WHERE tv_shows.id = tv_shows_genre.id
        ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
