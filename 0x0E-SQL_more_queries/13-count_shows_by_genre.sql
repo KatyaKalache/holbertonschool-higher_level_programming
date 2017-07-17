99c53c38cd962fe3af702b4d27e20e451276ba26
KatyaKalache <katya@kalache.fr>
2017-07-17 22:56:23 +0000

    joining tv_genres id with tv show genres genre id

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,6 +2,6 @@
 SELECT tv_genres.name AS genre,
 COUNT(tv_show_genres.show_id) AS number_shows
 FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.show_id
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.id
 ORDER BY number_shows DESC;
