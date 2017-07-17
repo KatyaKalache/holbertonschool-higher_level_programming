72eff98a937f1831fec8c1f658d27419e5558c1d
KatyaKalache <katya@kalache.fr>
2017-07-17 03:45:54 +0000

    order by asc

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- lists all the cities of California that can be found in hbtn_0d_usa
-SELECT id, name from cities WHERE state_id in(SELECT id from states WHERE state_id = 1);
+SELECT id, name from cities WHERE state_id in(SELECT id from states WHERE state_id = 1) ORDER BY cities.id ASC;
