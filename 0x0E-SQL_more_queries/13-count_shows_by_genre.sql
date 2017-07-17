4403b92afd8bb92f2e135ad1ce2d49182cd04190
KatyaKalache <katya@kalache.fr>
2017-07-17 22:26:35 +0000

    add order by desc

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,7 @@
 -- lists all genres and displays the number of shows linked to each
-SELECT tv_genres.name,
-COUNT(*)
+SELECT tv_genres.name AS genre,
+COUNT(*) AS number_shows
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.show_id
-GROUP BY tv_genres.name;
+GROUP BY tv_genres.name
+ORDER BY number_shows DESC;
