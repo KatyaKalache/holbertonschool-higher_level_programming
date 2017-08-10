146f42f012c7e2cfabf50be302e6ff5f9a257c76
KatyaKalache <katya@kalache.fr>
2017-08-10 23:55:51 +0000

    adding a line so the code is not executed when imported

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 # lists all states from the database hbtn_0e_0_usa
-import MySQLdb
-db = MySQLdb.connect(user="root", passwd="root", db="hbtn_0e_0_usa")
-cursor = db.cursor()
-cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
-data = cursor.fetchone()
-while data is not None:
-    print(data)
+if __name__ == "__main__":
+    import MySQLdb
+    db = MySQLdb.connect(user="root", passwd="root", db="hbtn_0e_0_usa")
+    cursor = db.cursor()
+    cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
     data = cursor.fetchone()
-db.close()
+    while data is not None:
+        print(data)
+        data = cursor.fetchone()
+    db.close()
