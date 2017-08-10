2e99d492f96c41107e0f0195564320ffee6779b1
KatyaKalache <katya@kalache.fr>
2017-08-10 23:51:46 +0000

    set password for mysql

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # lists all states from the database hbtn_0e_0_usa
 import MySQLdb
-db = MySQLdb.connect(user="root", db="hbtn_0e_0_usa")
+db = MySQLdb.connect(user="root", passwd="root", db="hbtn_0e_0_usa")
 cursor = db.cursor()
 cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
 data = cursor.fetchone()
