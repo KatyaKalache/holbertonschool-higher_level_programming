02270a0c71aeb9a90e12440dd3f0a88378031fe4
KatyaKalache <katya@kalache.fr>
2017-08-11 00:06:01 +0000

    adding host, port and not hardcoding the arguments

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,11 +2,14 @@
 # lists all states from the database hbtn_0e_0_usa
 if __name__ == "__main__":
     import MySQLdb
-    db = MySQLdb.connect(user="root", passwd="root", db="hbtn_0e_0_usa")
+    import sys
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
     data = cursor.fetchone()
     while data is not None:
         print(data)
         data = cursor.fetchone()
+    cursor.close()
     db.close()
