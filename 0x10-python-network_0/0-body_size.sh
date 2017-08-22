892cb376f3fddbb89084dc0ee8b8fec487623ee2
KatyaKalache <katya@kalache.fr>
2017-08-22 20:02:30 +0000

    putting back argument intake

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  takes in a URL and displays the size of the body of the response
-curl -si 0.0.0:5000 | grep Content-Length: | cut -d ":" -f2 | cut -d " " -f2
+curl -si $1 | grep Content-Length: | cut -d ":" -f2 | cut -d " " -f2
