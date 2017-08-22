5a6020306186f462ca4dd04fa887b3b57436dbf6
KatyaKalache <katya@kalache.fr>
2017-08-22 19:54:01 +0000

    changing to cutting the field methid

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  takes in a URL and displays the size of the body of the response
-curl -s -w \%{size_header} -o /dev/null $1
+curl -si 0.0.0:5000 | grep Content-Length: | cut -d ":" -f2
