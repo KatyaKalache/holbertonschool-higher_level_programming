2e19bc70a0c7d2942a1c6294965fc6f526170bfe
KatyaKalache <katya@kalache.fr>
2017-08-22 21:45:32 +0000

    adding argument for the url

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # POST request to the passed URL, using variables dispalys response
 curl -X POST -d 'email=hr@holbertonschool.com' -d \
-    'subject=I will always be here for PLD'
+    'subject=I will always be here for PLD' "$1"
