32240a8ed5d4a896e03804b589c1fca6b7c05ec1
KatyaKalache <katya@kalache.fr>
2017-08-29 05:45:45 +0000

    rm space after print

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,4 +5,4 @@ import sys
 if __name__ == "__main__":
     with urllib.request.urlopen(sys.argv[1]) as response:
         req = response.info()
-        print (req['x-request-id'])
+        print(req['x-request-id'])
