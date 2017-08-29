dc501e583859f0342ffb74156b389d4e5f6d075f
KatyaKalache <katya@kalache.fr>
2017-08-29 04:18:46 +0000

    code not executed when imported

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,9 +2,10 @@
 # fetches url
 import requests
 import json
-req = requests.get('https://intranet.hbtn.io/status')
-print ("Body response:")
-print ("    - type: ", end="")
-print (type(str(req)))
-print ("    - content: ", end="")
-print (req.text)
+if __name__ == "__main__":
+    req = requests.get('https://intranet.hbtn.io/status')
+    print ("Body response:")
+    print ("    - type: ", end="")
+    print (type(str(req)))
+    print ("    - content: ", end="")
+    print (req.text)
