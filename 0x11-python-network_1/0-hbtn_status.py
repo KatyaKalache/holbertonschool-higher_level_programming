6c47761cf96c3317a05e3c32a57e9b2ffdd5eb3b
KatyaKalache <katya@kalache.fr>
2017-08-29 04:44:40 +0000

    adding new line after import

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # fetches the url
 import urllib.request
+
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
