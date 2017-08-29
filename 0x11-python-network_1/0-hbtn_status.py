c0c7604c887363482b4cea054d1a43026ae54162
KatyaKalache <katya@kalache.fr>
2017-08-29 04:20:41 +0000

    initial

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 # fetches the url
 import urllib.request
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
-print ("Body response:")
-print ("    - type: ", end="")
-print (type(html))
-print ("    - content: ", end="")
-print (html)
-print ("    - utf8 content: ", end="")
-print (html.decode("utf-8"))
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print ("Body response:")
+        print ("    - type: ", end="")
+        print (type(html))
+        print ("    - content: ", end="")
+        print (html)
+        print ("    - utf8 content: ", end="")
+        print (html.decode("utf-8"))
