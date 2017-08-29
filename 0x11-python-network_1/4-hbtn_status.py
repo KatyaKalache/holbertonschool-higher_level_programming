8dba5cf8cdaa4b491f6567abf5eaeaff1468ad25
KatyaKalache <katya@kalache.fr>
2017-08-29 04:30:47 +0000

    adding \t

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@ import json
 if __name__ == "__main__":
     req = requests.get('https://intranet.hbtn.io/status')
     print ("Body response:")
-    print ("    - type: ", end="")
+    print ("\t- type: ", end="")
     print (type(str(req)))
-    print ("    - content: ", end="")
+    print ("\t- content: ", end="")
     print (req.text)
