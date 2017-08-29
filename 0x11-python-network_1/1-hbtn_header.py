970a65c71b6952f6c5802e5fff8df8153beeee6f
KatyaKalache <katya@kalache.fr>
2017-08-29 05:39:20 +0000

    prevent code from executing when import

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,6 +2,8 @@
 # retrieving request id
 import urllib.request
 import sys
-with urllib.request.urlopen(sys.argv[1]) as response:
-    req = response.info()
-print (req['x-request-id'])
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        req = response.info()
+        print (req['x-request-id'])
