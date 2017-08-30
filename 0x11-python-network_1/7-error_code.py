ad7f5c45fe7a13fdac99436ff4d039a44eddeed2
KatyaKalache <katya@kalache.fr>
2017-08-30 06:21:12 +0000

    greater than 399, so 400 included

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -4,7 +4,7 @@ from sys import argv
 
 if __name__ == "__main__":
     req = requests.get(argv[1])
-    if req.status_code < 400:
+    if req.status_code < 399:
         print(req.text)
     else:
         print("Error code: ", req.status_code)
