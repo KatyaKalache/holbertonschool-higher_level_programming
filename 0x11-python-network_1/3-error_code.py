e8b9e6b304c5000da2c34fd051107352232952f6
KatyaKalache <katya@kalache.fr>
2017-08-30 04:51:22 +0000

    making not executable when imported

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,9 +3,10 @@ import urllib.request
 from sys import argv
 from urllib.error import HTTPError
 
-try:
-    with urllib.request.urlopen(argv[1]) as response:
-        req = response.read()
-        print(req.decode("utf-8"))
-except HTTPError as e:
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(argv[1]) as response:
+            req = response.read()
+            print(req.decode("utf-8"))
+    except HTTPError as e:
         print('Error code:', e.code)
