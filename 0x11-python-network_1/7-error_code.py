1f51ccc4143fe2638397966cca826bf57eba9161
KatyaKalache <katya@kalache.fr>
2017-08-30 06:13:32 +0000

    initial

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    req = requests.get(argv[1])
+    if req.status_code < 400:
+        print(req.text)
+    else:
+        print("Error code: ", req.status_code)
