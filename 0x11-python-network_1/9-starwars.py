dfcce017166f35deb3a8322922082f68b24aa6d8
KatyaKalache <katya@kalache.fr>
2017-08-31 01:46:22 +0000

    initial

---
+++ 0x11-python-network_1/9-starwars.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    req = requests.get('https://swapi.co/api/people/',
+                       params={'search': argv[1]})
+    json_format = req.json()
+    results = json_format.get('results')
+    count = json_format.get('count')
+    print("Number of result:", count)
+    for i in results:
+        print(i.get('name'))
