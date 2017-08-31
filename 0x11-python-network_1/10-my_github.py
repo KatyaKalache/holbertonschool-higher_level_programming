bb70b3350e103aaf03392c1d05de73a277134d30
KatyaKalache <katya@kalache.fr>
2017-08-31 05:53:43 +0000

    rm slash at end of url

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -3,7 +3,7 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    req = requests.get('https://api.github.com/user/',
+    req = requests.get('https://api.github.com/user',
                        auth=(argv[1], argv[2]))
     json_format = req.json()
     print(json_format.get('id'))
