995ed74de4528704e11d948e8259533e5dfc6cf4
KatyaKalache <katya@kalache.fr>
2017-08-31 04:44:38 +0000

    initial

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    req = requests.get('https://api.github.com/user/',
+                       auth=(argv[1], argv[2]))
+    json_format = req.json()
+    print(json_format.get('id'))
