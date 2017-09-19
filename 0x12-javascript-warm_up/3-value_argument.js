7bc1cace9b1e4f5248b89d9310573db30b2ea4b9
KatyaKalache <katya@kalache.fr>
2017-09-19 23:45:45 +0000

    can't use length to check number of argv, so checking if index 2 is undefined

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Prints the first argument passed to it
 const messageNotFound = 'No argument'
-if (process.argv.length <= 2) {
+if (process.argv[2] === undefined) {
   console.log(messageNotFound);
 } else {
   console.log(String(process.argv.slice(2)));
