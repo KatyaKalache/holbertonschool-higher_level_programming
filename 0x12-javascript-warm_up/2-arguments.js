afe641bd849a05f037cce0f6818dc5d314d565a5
KatyaKalache <katya@kalache.fr>
2017-09-19 23:52:02 +0000

    adding message for plural

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
 // Prints a message depending of the number of arguments passed
 const messageFound = 'Argument found';
+const messageFoundPlural = 'Arguments found';
 const messageNotFound = 'No argument';
 if (process.argv[2] === undefined) {
   console.log(messageNotFound);
-} else {
+} else if (process.argv[3] === undefined) {
   console.log(messageFound);
+} else {
+  console.log(messageFoundPlural);
 }
