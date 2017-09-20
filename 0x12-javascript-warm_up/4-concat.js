c4036190faada76ac01dd8abbfca1935568b10c6
KatyaKalache <katya@kalache.fr>
2017-09-20 00:16:10 +0000

    replacing tab with 2 spaces

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 // prints two arguments passed to it separated by "is"
 console.log(String(process.argv[2]) + ' ' + 'is' + ' ' +
-	    String(process.argv[3]));
+  String(process.argv[3]));
