6b16420e8885406e8c5222a45c519ff744be8ab9
KatyaKalache <katya@kalache.fr>
2017-09-20 00:12:35 +0000

    adding description and changing fisrt line form ../end nodejs to /node

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,3 +1,4 @@
-#!/usr/bin/env nodejs
+#!/usr/bin/node
+// prints two arguments passed to it separated by "is"
 console.log(String(process.argv[2]) + ' ' + 'is' + ' ' +
 	    String(process.argv[3]));
