1b6eb7da5ec390724b025a04a4d8c9222883f60e
KatyaKalache <katya@kalache.fr>
2017-09-26 23:23:38 +0000

    initial

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Returns status code
+const request = require('request');
+const url = 'http://swapi.co/api/films/';
+const argument = process.argv[2];
+request.get(url + argument, function (err, res, body) {
+  if (err) {
+    return console.log(err);
+  }
+  let jsonFormat = JSON.parse(body);
+  let title = jsonFormat['title'];
+  console.log(title);
+});
