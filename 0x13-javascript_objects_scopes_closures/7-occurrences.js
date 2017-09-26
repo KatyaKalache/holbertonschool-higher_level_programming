ddcb2d8e69fc3366130a22344e2db54bfccd0c9e
KatyaKalache <katya@kalache.fr>
2017-09-26 05:10:51 +0000

    moving let inside function scope

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // Returns the number of occurrences in a list
 exports.nbOccurences = function (list, searchElement) {
-  let counter = 0;
-  let matchCount = 0;
   for (; counter < list.length; counter++) {
+    let counter = 0;
+    let matchCount = 0;
     if (searchElement === list[counter]) {
       matchCount += 1;
       return matchCount;
