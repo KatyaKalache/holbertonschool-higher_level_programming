a32c3f623c6815c6878f477a73b11c5ad5626d27
KatyaKalache <katya@kalache.fr>
2017-09-27 01:30:35 +0000

    initial

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+// Returns status code
+const request = require('request');
+const url = 'http://swapi.co/api/films/';
+const wedgeUrl = 'https://swapi.co/api/people/18/';
+let i = 0;
+let matchCount = 0;
+request.get(url, function (err, res, results) {
+  if (err) {
+    return console.log(err);
+  }
+  let jsonFormat = JSON.parse(results);
+  let movieList = jsonFormat['results'];
+  for (; i < movieList.length; i++) {
+    let charList = movieList[i]['characters'];
+    for (let j = 0; j < charList.length; j++) {
+      if (charList[j] === wedgeUrl) {
+        matchCount++;
+      }
+    }
+  }
+  console.log(matchCount);
+});
