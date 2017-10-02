6d695ca19e2e2f21dc669168e370f4831574a984
KatyaKalache <katya@kalache.fr>
2017-10-02 05:03:06 +0000

    rm read as function name from read method, defining result

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 // Concats text from 2 files and write it to the third one
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function read (err, dataA) {
+fs.readFile(process.argv[2], 'utf8', function (err, dataA) {
   if (err) {
     return console.log(err);
   }
-  fs.readFile(process.argv[3], 'utf8', function read (err, dataB) {
+  fs.readFile(process.argv[3], 'utf8', function (err, dataB) {
     if (err) {
       return console.log(err);
     }
-    result = dataA + '\n' + dataB + '\n';
+    let result = dataA + '\n' + dataB + '\n';
     fs.writeFile(process.argv[4], result, 'utf8', function (err, data) {
       if (err) {
         return console.log(err);
