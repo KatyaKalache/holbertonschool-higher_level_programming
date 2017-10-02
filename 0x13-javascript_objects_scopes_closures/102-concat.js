8836c1ca98529df17a6cf6265c6623bd372aeee9
KatyaKalache <katya@kalache.fr>
2017-10-02 04:50:23 +0000

    initial

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// Concats text from 2 files and write it to the third one
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function read (err, dataA) {
+  if (err) {
+    return console.log(err);
+  }
+  fs.readFile(process.argv[3], 'utf8', function read (err, dataB) {
+    if (err) {
+      return console.log(err);
+    }
+    fs.writeFile(process.argv[4], dataA + '\n' + dataB + '\n');
+  });
+});
